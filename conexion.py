99f16e72df15aa5084f2bbbf800f967a8352c433
Milagros Dominguez <dym.milagros.dominguez@gmail.com>
2026-03-12 11:39:45 +0000

    informe_final

---
+++ conexion.py
@@ -0,0 +1,14 @@
+import xmlrpc.client
+
+url = "http://localhost:8069"
+db = "odoo_dev"
+username = "admin"
+password = "1234"   # o una API key
+
+# 1. Conexión al servicio de autenticación
+common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
+
+# 2. Autenticación
+uid = common.authenticate(db, username, password, {})
+
+print("UID obtenido:", uid)
